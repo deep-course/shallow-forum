02bf87c06a43bca473f841891cd8bddba8e878e8
zergtant <tant@deep-course.ai>
2019-09-30 11:36:14 +0800

    添加一个image

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.6.42-log - Homebrew
--- 服务器操作系统:                      osx10.14
--- HeidiSQL 版本:                  9.5.0.5196
+-- Host:                         127.0.0.1
+-- Server version:               5.7.27 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,7 +11,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出  表 shu.board_comment 结构
+-- Dumping structure for table shu.board_comment
+DROP TABLE IF EXISTS `board_comment`;
 CREATE TABLE IF NOT EXISTS `board_comment` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(10) unsigned NOT NULL COMMENT 'postid',
@@ -26,12 +27,12 @@ CREATE TABLE IF NOT EXISTS `board_comment` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评论表';
 
--- 正在导出表  shu.board_comment 的数据：~0 rows (大约)
-DELETE FROM `board_comment`;
+-- Dumping data for table shu.board_comment: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_comment` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_comment` ENABLE KEYS */;
 
--- 导出  表 shu.board_post 结构
+-- Dumping structure for table shu.board_post
+DROP TABLE IF EXISTS `board_post`;
 CREATE TABLE IF NOT EXISTS `board_post` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `slug` char(32) NOT NULL COMMENT '唯一标示',
@@ -48,12 +49,12 @@ CREATE TABLE IF NOT EXISTS `board_post` (
   UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='帖子';
 
--- 正在导出表  shu.board_post 的数据：~0 rows (大约)
-DELETE FROM `board_post`;
+-- Dumping data for table shu.board_post: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_post` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_post` ENABLE KEYS */;
 
--- 导出  表 shu.board_postacticity 结构
+-- Dumping structure for table shu.board_postacticity
+DROP TABLE IF EXISTS `board_postacticity`;
 CREATE TABLE IF NOT EXISTS `board_postacticity` (
   `post_id` int(11) unsigned NOT NULL,
   `lastcommentuser_id` int(11) unsigned NOT NULL COMMENT '最后回复的用户id',
@@ -65,12 +66,27 @@ CREATE TABLE IF NOT EXISTS `board_postacticity` (
   PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='帖子信息';
 
--- 正在导出表  shu.board_postacticity 的数据：~0 rows (大约)
-DELETE FROM `board_postacticity`;
+-- Dumping data for table shu.board_postacticity: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_postacticity` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_postacticity` ENABLE KEYS */;
 
--- 导出  表 shu.board_postintag 结构
+-- Dumping structure for table shu.board_postimage
+DROP TABLE IF EXISTS `board_postimage`;
+CREATE TABLE IF NOT EXISTS `board_postimage` (
+  `id` int(10) unsigned NOT NULL,
+  `hash` char(50) NOT NULL COMMENT '图片的hash值',
+  `post_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `addtime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='图片';
+
+-- Dumping data for table shu.board_postimage: ~0 rows (approximately)
+/*!40000 ALTER TABLE `board_postimage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board_postimage` ENABLE KEYS */;
+
+-- Dumping structure for table shu.board_postintag
+DROP TABLE IF EXISTS `board_postintag`;
 CREATE TABLE IF NOT EXISTS `board_postintag` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` int(11) unsigned DEFAULT NULL,
@@ -79,12 +95,12 @@ CREATE TABLE IF NOT EXISTS `board_postintag` (
   UNIQUE KEY `post_id_tag_id` (`post_id`,`tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  shu.board_postintag 的数据：~0 rows (大约)
-DELETE FROM `board_postintag`;
+-- Dumping data for table shu.board_postintag: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_postintag` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_postintag` ENABLE KEYS */;
 
--- 导出  表 shu.board_setting 结构
+-- Dumping structure for table shu.board_setting
+DROP TABLE IF EXISTS `board_setting`;
 CREATE TABLE IF NOT EXISTS `board_setting` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(100) NOT NULL,
@@ -92,12 +108,12 @@ CREATE TABLE IF NOT EXISTS `board_setting` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='配置表，配置在初始化的时候写入json文件，定期更新';
 
--- 正在导出表  shu.board_setting 的数据：~0 rows (大约)
-DELETE FROM `board_setting`;
+-- Dumping data for table shu.board_setting: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_setting` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_setting` ENABLE KEYS */;
 
--- 导出  表 shu.board_tag 结构
+-- Dumping structure for table shu.board_tag
+DROP TABLE IF EXISTS `board_tag`;
 CREATE TABLE IF NOT EXISTS `board_tag` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL COMMENT '名称',
@@ -109,15 +125,15 @@ CREATE TABLE IF NOT EXISTS `board_tag` (
   UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='标签';
 
--- 正在导出表  shu.board_tag 的数据：~1 rows (大约)
-DELETE FROM `board_tag`;
+-- Dumping data for table shu.board_tag: ~2 rows (approximately)
 /*!40000 ALTER TABLE `board_tag` DISABLE KEYS */;
 INSERT INTO `board_tag` (`id`, `name`, `type`, `slug`, `info`, `color`) VALUES
 	(1, '测试tag', 'main', 'test', '测试的tag', ''),
 	(2, '子tag', 'sub', 'tag1', '目前没有区分子tag', '');
 /*!40000 ALTER TABLE `board_tag` ENABLE KEYS */;
 
--- 导出  表 shu.board_tagactivity 结构
+-- Dumping structure for table shu.board_tagactivity
+DROP TABLE IF EXISTS `board_tagactivity`;
 CREATE TABLE IF NOT EXISTS `board_tagactivity` (
   `tag_id` int(11) unsigned NOT NULL,
   `lastdposttime` datetime NOT NULL COMMENT '最后发布的时间',
@@ -128,15 +144,15 @@ CREATE TABLE IF NOT EXISTS `board_tagactivity` (
   PRIMARY KEY (`tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签相关的活动';
 
--- 正在导出表  shu.board_tagactivity 的数据：~2 rows (大约)
-DELETE FROM `board_tagactivity`;
+-- Dumping data for table shu.board_tagactivity: ~2 rows (approximately)
 /*!40000 ALTER TABLE `board_tagactivity` DISABLE KEYS */;
 INSERT INTO `board_tagactivity` (`tag_id`, `lastdposttime`, `lastcommenttime`, `lastpost_id`, `lastcommentpost_id`, `postcount`) VALUES
 	(1, '2019-05-08 15:25:29', '0000-00-00 00:00:00', 3, 0, 4),
 	(2, '2019-05-08 15:50:42', '0000-00-00 00:00:00', 7, 0, 6);
 /*!40000 ALTER TABLE `board_tagactivity` ENABLE KEYS */;
 
--- 导出  表 shu.board_useruppost 结构
+-- Dumping structure for table shu.board_useruppost
+DROP TABLE IF EXISTS `board_useruppost`;
 CREATE TABLE IF NOT EXISTS `board_useruppost` (
   `id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
@@ -146,12 +162,12 @@ CREATE TABLE IF NOT EXISTS `board_useruppost` (
   UNIQUE KEY `user_id_post_id` (`user_id`,`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户点赞表';
 
--- 正在导出表  shu.board_useruppost 的数据：~0 rows (大约)
-DELETE FROM `board_useruppost`;
+-- Dumping data for table shu.board_useruppost: ~0 rows (approximately)
 /*!40000 ALTER TABLE `board_useruppost` DISABLE KEYS */;
 /*!40000 ALTER TABLE `board_useruppost` ENABLE KEYS */;
 
--- 导出  表 shu.user_activity 结构
+-- Dumping structure for table shu.user_activity
+DROP TABLE IF EXISTS `user_activity`;
 CREATE TABLE IF NOT EXISTS `user_activity` (
   `user_id` int(11) unsigned NOT NULL,
   `lastactiontime` datetime NOT NULL COMMENT '最后活动时间',
@@ -162,14 +178,14 @@ CREATE TABLE IF NOT EXISTS `user_activity` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户活动';
 
--- 正在导出表  shu.user_activity 的数据：~0 rows (大约)
-DELETE FROM `user_activity`;
+-- Dumping data for table shu.user_activity: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_activity` DISABLE KEYS */;
 INSERT INTO `user_activity` (`user_id`, `lastactiontime`, `lastlogintime`, `logincount`, `postcount`, `commentcount`) VALUES
 	(1, '2019-05-08 16:28:14', '2019-05-08 16:16:25', 3, 8, 0);
 /*!40000 ALTER TABLE `user_activity` ENABLE KEYS */;
 
--- 导出  表 shu.user_author 结构
+-- Dumping structure for table shu.user_author
+DROP TABLE IF EXISTS `user_author`;
 CREATE TABLE IF NOT EXISTS `user_author` (
   `user_id` int(11) unsigned NOT NULL,
   `slug` varchar(50) NOT NULL COMMENT 'url',
@@ -179,12 +195,12 @@ CREATE TABLE IF NOT EXISTS `user_author` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户作者表，在这个表里面的是公众号等自媒体';
 
--- 正在导出表  shu.user_author 的数据：~0 rows (大约)
-DELETE FROM `user_author`;
+-- Dumping data for table shu.user_author: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_author` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_author` ENABLE KEYS */;
 
--- 导出  表 shu.user_group 结构
+-- Dumping structure for table shu.user_group
+DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE IF NOT EXISTS `user_group` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL COMMENT '组名',
@@ -192,8 +208,7 @@ CREATE TABLE IF NOT EXISTS `user_group` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户组，没有组的都是普通用户';
 
--- 正在导出表  shu.user_group 的数据：~3 rows (大约)
-DELETE FROM `user_group`;
+-- Dumping data for table shu.user_group: ~3 rows (approximately)
 /*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
 INSERT INTO `user_group` (`id`, `name`, `color`) VALUES
 	(1, '管理员', ''),
@@ -201,7 +216,8 @@ INSERT INTO `user_group` (`id`, `name`, `color`) VALUES
 	(3, '嘉宾', '');
 /*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 
--- 导出  表 shu.user_token 结构
+-- Dumping structure for table shu.user_token
+DROP TABLE IF EXISTS `user_token`;
 CREATE TABLE IF NOT EXISTS `user_token` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL COMMENT '用户的id',
@@ -212,12 +228,12 @@ CREATE TABLE IF NOT EXISTS `user_token` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户token表，自动清除过期的';
 
--- 正在导出表  shu.user_token 的数据：~0 rows (大约)
-DELETE FROM `user_token`;
+-- Dumping data for table shu.user_token: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_token` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_token` ENABLE KEYS */;
 
--- 导出  表 shu.user_user 结构
+-- Dumping structure for table shu.user_user
+DROP TABLE IF EXISTS `user_user`;
 CREATE TABLE IF NOT EXISTS `user_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL COMMENT '用户名',
@@ -231,14 +247,14 @@ CREATE TABLE IF NOT EXISTS `user_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
--- 正在导出表  shu.user_user 的数据：~0 rows (大约)
-DELETE FROM `user_user`;
+-- Dumping data for table shu.user_user: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_user` DISABLE KEYS */;
 INSERT INTO `user_user` (`id`, `username`, `email`, `phone`, `activate`, `password`, `jointime`, `bio`, `lock`) VALUES
 	(1, 'tant', 'tant@123.com', '123456', 1, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2019-05-01 10:00:00', '', 0);
 /*!40000 ALTER TABLE `user_user` ENABLE KEYS */;
 
--- 导出  表 shu.user_userinfo 结构
+-- Dumping structure for table shu.user_userinfo
+DROP TABLE IF EXISTS `user_userinfo`;
 CREATE TABLE IF NOT EXISTS `user_userinfo` (
   `user_id` int(10) unsigned NOT NULL,
   `key` varchar(50) NOT NULL,
@@ -246,12 +262,12 @@ CREATE TABLE IF NOT EXISTS `user_userinfo` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息，以用户属性的KV的形式存储';
 
--- 正在导出表  shu.user_userinfo 的数据：~0 rows (大约)
-DELETE FROM `user_userinfo`;
+-- Dumping data for table shu.user_userinfo: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_userinfo` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_userinfo` ENABLE KEYS */;
 
--- 导出  表 shu.user_useringroup 结构
+-- Dumping structure for table shu.user_useringroup
+DROP TABLE IF EXISTS `user_useringroup`;
 CREATE TABLE IF NOT EXISTS `user_useringroup` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
@@ -261,8 +277,7 @@ CREATE TABLE IF NOT EXISTS `user_useringroup` (
   UNIQUE KEY `user_id_group_id` (`user_id`,`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户分组对应';
 
--- 正在导出表  shu.user_useringroup 的数据：~0 rows (大约)
-DELETE FROM `user_useringroup`;
+-- Dumping data for table shu.user_useringroup: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user_useringroup` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_useringroup` ENABLE KEYS */;
 
