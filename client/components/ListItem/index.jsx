89633c1667d774dfda00e0cc9715c1087e752c97
harper <569952471@qq.com>
2019-12-17 23:49:54 +0800

    首页列表

--- client/components/ListItem/index.jsx
+++ client/components/ListItem/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
+import Router from 'next/router';
 import { Icon } from 'antd'
 import moment from 'dayjs'
 import './index.less'
@@ -10,19 +11,26 @@ class PageHead extends Component {
   constructor(props) {
     super(props)
   }
+
+  usernameClick = () => {
+    Router.push('/detail')
+  }
+
   render() {
     const { taglist, sort } = this.props.global
     const data = this.props.data
+    console.log(data)
     return (
       <div className="slug-list-item">
         <div className="slug-list-item-left">
           <div className="slug-list-item-info">
-            <span className="slug-list-item-type">{data.label}</span>
-            <span className="slug-list-item-name">{data.username}</span>
+            {/* <span className="slug-list-item-type">{data.label}</span> */}
+            <span className="slug-list-item-name" onClick={this.usernameClick}>{data.username}</span>
+            <span>·</span>
             <span className="slug-list-item-time">{moment(new Date(data.pubtime)).format('YYYY/MM/DD HH:mm:ss')}</span>
-            <span className="slug-list-item-label">前端</span>
+            {/* <span className="slug-list-item-label">前端</span> */}
           </div>
-          <div className="slug-list-item-title">{data.title}</div>
+          <div className="slug-list-item-title">【{data.slug}】{data.title}</div>
           <div className="slug-list-item-action">
             <span className="slug-list-item-action-item">
               <Icon type="like" />
